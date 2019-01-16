80cfaf1fab953e58c9817496855eda3a8e28e33c
katoussa <36401674+katoussa@users.noreply.github.com>
2019-01-16 21:31:20 +0100

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -56,6 +56,7 @@ var form = {
                 form.signature.className = "signatureVisible";
             };
             sign.noDraw();
+            sign.btn.className = "invisible";
         });
     }
 };
